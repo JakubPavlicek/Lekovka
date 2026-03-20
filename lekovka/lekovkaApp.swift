ea8239b8e0f55aac4e9fd6006ab2498bfc99442d
Jakub Pavlíček <kuba.cb@seznam.cz>
2026-03-20 22:26:32 +0100

    Initial Commit

---
+++ lekovka/lekovkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  lekovkaApp.swift
+//  lekovka
+//
+//  Created by Jakub Pavlíček on 20.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct lekovkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
